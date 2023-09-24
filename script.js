240604c2e6b0f21a0f29b2710e9b70cda92fa17e
Prateek <107943022+Prat0903@users.noreply.github.com>
2023-09-24 20:31:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+/* Whenever we use querySelector method with a selector which actually matches multiple elements then only the first one will get selected.
+
+const btnsOpenModal = document.querySelector('.show-modal');
+console.log(btnsOpenModal); */
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const showModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', showModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    console.log(e.key);
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+})
